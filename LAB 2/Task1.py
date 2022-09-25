f0a430d4b34de6b366977ccf0d53b2b5014ba97c
valentinn cruz <valentinnocruz@gmail.com>
2022-09-24 18:13:26 -0700

    Found Max Pixel value alternate way

--- LAB 2/Task1.py
+++ LAB 2/Task1.py
@@ -19,15 +19,27 @@ im_gray.show()
 
 # Get access to the pixel values through the matrix im_gray_pixels.
 im_gray_pixels = asarray(im_gray)
+rows, cols = im_gray_pixels.shape
+
+for row in range(rows):
+    for col in range(cols):
+        # get the current pixel value
+        current_pixel_value = im_gray_pixels[row, col]
+        # Manipulating your pixel values
+        # for example: print pixel values that are greater than 200
+        if current_pixel_value >= 240:
+            print("Max Pixel Value is: ", current_pixel_value)
 
 
-# print out max pixel value 
-biggest = np.amax(im_gray_pixels)
-print("the max pixel value is", biggest)
 
 
-a = np.array(im_gray)
-print(np.transpose(a))
+# # print out max pixel value 
+# biggest = np.amax(im_gray_pixels)
+# print("the max pixel value is", biggest)
+
+
+# a = np.array(im_gray)
+# print(np.transpose(a))
 #np.rot90(im_gray, k=2, axes=(0,1) )
 #im_gray.show()
 
