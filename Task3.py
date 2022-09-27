26d96b168dd8426e89540fe07dba612be7a6d693
valentinn cruz <valentinnocruz@gmail.com>
2022-09-26 21:55:01 -0700

    Complete

--- Task3.py
+++ Task3.py
@@ -3,6 +3,7 @@ from PIL import Image, ImageOps
 # Import numpy
 import numpy as np
 from numpy import asarray
+import math
 
 # The size of the gradient image.
 rows = 100
@@ -27,7 +28,33 @@ image_pix = asarray(a) # set as an array
 newImage = Image.fromarray(np.uint8(image_pix))
 
 
+
+
+#--------------------------
+# get average pixel value
+#--------------------------
+x = np.size(image_pix, 0)
+y = np.size(image_pix, 1)
+
+
+
+total =0
+for i in range(x):
+    for j in range(y):
+        total+=image_pix[(i,j)]
+    b=x*y
+mean=(total)/b
+print("Mean Value is: ",np.mean(mean))
+
 newImage.show()
 newImage.save('image.tif')
 
+
+# total =0
+# for i in range(0, cols):
+#     for j in range(0, rows):
+#         total+= image_pix.getpixel((i,j))[0]
+
+# mean = total/(rows * cols)
+# print(mean)
 
