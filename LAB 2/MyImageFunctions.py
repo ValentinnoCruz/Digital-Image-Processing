151b8fbd5516909f7e588a06701ca48898eab1d9
valentinn cruz <valentinnocruz@gmail.com>
2022-10-03 20:41:25 -0700

    made changes

--- LAB 2/MyImageFunctions.py
+++ LAB 2/MyImageFunctions.py
@@ -10,27 +10,18 @@ def myImageInverse( inImage_pixels ):
 
 
 
-# # This function takes as input a numpy matrix representing a grayscale image and
-# # outputs another numpy matrix which is the image inverse of the input.
-# # That is, for each pixel, output_value = 255 - input_value
+    # compute size of the matrix
+    cols = np.size(inImage_pixels, 0)
+    rows = np.size(inImage_pixels, 1)
 
-# Syntax:
-# out_numpy_matrix = myImageInverse( in_numpy_matrix )
+    # emulate matrix numpy of the same exact size
+    outImage = np.zeros((cols,rows), dtype=int)
 
-# Input:
-# in_numpy_matrix = the grayscale values of the input image
+    for col in range(cols): # traverse through i
+        for row in range(rows): # traverse through j
+            # now we get the difference between the two
+            outImage[col][row] = 255 - inImage_pixels[col][row]
 
-# Output:
-# out_numpy_matrix = the grayscale of the inverse image
 
-    # determine the size of input matrix
-    x = np.size(inImage_pixels, 0)
-    y = np.size(inImage_pixels, 1)
-    # created new numpy matrix of the same size
-    outImage = np.zeros((x,y), dtype=int)
-    # applying the formula
-    for i in range(x):
-        for j in range(y):
-            outImage[i][j] = 255 - inImage_pixels[i][j]
-    # returning the output image
+    # return the image
     return outImage;
