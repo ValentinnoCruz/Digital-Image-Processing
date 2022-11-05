b3c15e5fcd446f56f4bca01723f06020d9934536
valentinn cruz <valentinnocruz@gmail.com>
2022-11-04 18:39:23 -0700

    moved updated files from lab 2

--- LAB 2/tempCodeRunnerFile.py
+++ LAB 2/tempCodeRunnerFile.py
@@ -1,9 +1,5 @@
 
-
-# total =0
-# for i in range(x):
-#     for j in range(y):
-#         total+=image_pix[(i,j)]
-#     b=x*y
-# mean=(total)/b
-# print("Mean Value is: ",np.mean(mean))
+# im_pixels = np.zeros(shape=(rows, cols), dtype=int)
+# for i in range(0, rows):
+#     for j in range(0, cols):
+#         im_pixels[i][j] = im_gray_rot[i][j]
