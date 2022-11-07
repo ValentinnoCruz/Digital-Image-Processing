dd4d0c6371f87207b5bcc1a31b326a8cf24120a2
valentinn cruz <valentinnocruz@gmail.com>
2022-11-06 22:30:42 -0800

    Fixed Compute Hist error

--- LAB 4/My_HE_functions.py
+++ LAB 4/My_HE_functions.py
@@ -17,7 +17,7 @@ def compute_histogram(image_pixels):
         for y in range(image_pixels.shape[1]):            
             hist[int(image_pixels[x][y])] += 1
             
-#     return hist
+    return hist
 #     # using ravel we represent our img as a 1-dimensional vector
 #     vectorized_image = image_pixels.ravel() 
 #     histogram = np.zeros(shape=(256)) 
@@ -49,65 +49,6 @@ def equalize( in_image_pixels ):
     eq_img = equalized_image.reshape(in_image_pixels.shape)
     return eq_img
 
-#     #Equalize 
-# #   Takes in as input a grayscale image 256 bits and returns the histogram
-# #   equalized version.
-    
-#     #gets index sizes
-#     mn=np.size(in_image_pixels)
-# # equalize.m:7
-#     m=mn*1
-# # equalize.m:8
-#     n=mn*2
-# # equalize.m:9
-#     #create a matrix for final processed image
-#     finalImage=np.zeros(m,n)
-# # equalize.m:12
-#     #store probability values on a 256 vector
-#     histogram=compute_histogram(in_image_pixels)
-# # equalize.m:15
-#     #get transformation vector that is size 256
-#     transformation=histogram_transform(compute_histogram(in_image_pixels))
-# # equalize.m:18
-#     #plot calculated values to final image
-#     for i in np.arange(1,m).reshape(-1):
-#         for j in np.arange(1,n).reshape(-1):
-#             finalImage[i,j]=transformation(in_image_pixels(i,j) + 1)
-# # equalize.m:23
-    
-#     #calculate histogram of final transformed image
-#     histogram2=compute_histogram(finalImage)
-# # equalize.m:28
-#     finalImage=np.uint8(finalImage)
-# # equalize.m:30
-#     #PLOT EVERYTHING
-#     #figure
-#     plt.subplot(2,2,1)
-#     plot_histogram(histogram)
-#     plt.subplot(2,2,2)
-#     plot_histogram(histogram2)
-#     plt.subplot(2,2,3)
-#     plt.imshow(in_image_pixels)
-#     plt.subplot(2,2,4)
-#     plt.imshow(finalImage)
-#     image=np.double(image)
-# # equalize.m:40
-#     finalImage=np.double(finalImage)
-# # equalize.m:41
-#     np.disp('Mean Origianl')
-#     np.disp(np.mean(np.ravel(image)))
-#     np.disp('Standard Deviation Origianl')
-#     np.disp(np.std(np.ravel(image)))
-#     np.disp('Mean Equalized')
-#     np.disp(np.mean(np.ravel(finalImage)))
-#     np.disp('Standard Deviation Equalized')
-#     np.disp(np.std(np.ravel(finalImage)))
-#     outputArg1=0
-# # equalize.m:55
-#     #PRINT MEAN AND STANDARD DEVIATION
-    
-#     return outputArg1
-
 
 
 def plot_histogram( hist ):
@@ -125,10 +66,6 @@ def plot_histogram( hist ):
     #
     # History:
     #   S. Newsam     10/23/2022   created
-
-
-
-
 
 
 
